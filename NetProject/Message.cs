704b256a7a98916353999bbd8f11d60859c504e9
alzavrmk <3845303@gmail.com>
2023-12-09 22:21:48 +0300

    add server programm

---
+++ NetProject/Message.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace NetServer
+{
+    public class Message
+    {
+        public string Text { get; set; }
+        public DateTime DateTime { get; set; }
+        public string NicknameFrom { get; set; }
+        public string NicknameTo { get; set; }
+
+        public string SerialazeMassageToJson() => JsonSerializer.Serialize(this);
+        public static Message DeserializeFromJson(string message) => JsonSerializer.Deserialize<Message>(message);
+
+        public void Print()
+        {
+            Console.WriteLine(ToString());
+        }
+
+        public override string ToString()
+        {
+            return $"{DateTime} получено сообщение {Text} от {NicknameFrom}";
+        }
+
+    }
+}
---
+++ NetProject/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace NetServer
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Server("Hello");
+        }
+
+        public void task1()
+        {
+            Message msg = new Message() 
+            { 
+                Text = "Hello", 
+                DateTime = DateTime.Now, 
+                NicknameFrom = "Artem",
+                NicknameTo = "All"
+            };
+
+            string json = msg.SerialazeMassageToJson();
+            Console.WriteLine(json);
+            Message? msgDeserialized = Message.DeserializeFromJson(json);
+        }
+
+        static void Server(string name)
+        {
+            UdpClient udpClient = new UdpClient(12345);
+
+            //IPEndPoint iPEndPoint = new IPEndPoint(System.Net.IPAddress.Parse("127.0.0.1"), 12345);
+            IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Any, 12345);
+            Console.WriteLine("Сервер ждет сообщение от клиента");
+
+            while (true)
+            {
+                byte[] buffer = udpClient.Receive(ref  iPEndPoint);
+
+                if (buffer == null) break;
+
+                var massageText = Encoding.UTF8.GetString(buffer);
+                Message message = Message.DeserializeFromJson(massageText);
+                message.Print();
+                Console.WriteLine( "Cообщение принял!");
+
+            }
+        }
+    }
+}
